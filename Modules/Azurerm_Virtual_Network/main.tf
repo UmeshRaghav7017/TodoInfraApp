291905e89a7e1207e1368f7cdb7a881a7edcc9d3
Anureshgit91 <anuresh89@gmail.com>
2025-07-16 23:26:21 +0530

    feature/20

--- Modules/Azurerm_Virtual_Network/main.tf
+++ Modules/Azurerm_Virtual_Network/main.tf
@@ -5,3 +5,11 @@ resource "azurerm_virtual_network" "Vnet1" {
     address_space = var.vnet-address_space
   
 }
+
+# 🔄 Summary (Hindi mai):
+# Pehle repo ko clone karo agar already nahi kiya hai.
+
+# git fetch se remote branches lao.
+
+# # git checkout feature se feature branch par switch karo. 
+# Agar branch local me nahi hai to git checkout -b feature origin/feature use karo.
